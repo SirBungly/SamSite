efe769cd1419ff7a9ea27e78636fbbd049d1b61d
SirBungly <sirbungly@gmail.com>
2025-08-06 21:04:32 +0100

    Fix 3

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://sirbungly.github.io/SamSite/',
+  base: 'SamSite',
   output: 'static',
   vite: {
     plugins: [tailwindcss()]
