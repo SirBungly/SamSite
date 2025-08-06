bde6629a86db8a5e6f5a7cfc31c026e7532c650f
SirBungly <sirbungly@gmail.com>
2025-08-06 20:26:24 +0100

    init

---
+++ src/assets/scripts/Zhelna.ts
@@ -0,0 +1,79 @@
+export let ZHELNA: string = `EXT. CRANE ARM JUTTING OVER THE CITY - NIGHT
+
+ZHELNA and DEKKA are perched, surveying the city from their vantage point. Both are wearing battle-tested HIGH-TECH ARMOUR with JETPACKS. Zhelna's bears the distinctive mark of a soldier of the MIDWIFE division, whilst Dekka's seems to be assembled from a selection of more worn items from various factions. The HUMDRUM of the city buzzes beneath them. The breeze catches a strand of Zhelna's BRAIDED HAIR and she tucks it back into place, frustrated.
+
+ZHELNA
+The invitation said they should have finished the symposium by now.
+
+DEKKA
+I mean we could've got the wrong exit.
+
+ZHELNA
+No, Cadmius definitely said it was gonna be here. And we've trusted him before. He's never been wrong.
+
+Zhelna stands, raising her SNIPER RIFLE to her face. The light from the SCOPE illuminates her black eyes.
+
+DEKKA
+(Hushed)
+Hey! Someone could see us.
+
+ZHELNA
+I've done this a million times before. If someone wanted to see us, they would already be right on top of us.
+
+DEKKA
+Sometimes your confidence... staggers me. You know if we get caught doing this we'll--
+
+Zhelna looks at Dekka, lowering the scope.
+
+ZHELNA
+(Sternly)
+You think I don't know the consequences of trying to infiltrate the upper city? That I somehow haven't factored them in?
+
+DEKKA
+You know what I mean. I'm just trying to...
+
+Dekka tails off. A slight commotion in the CROWD below as four MASKED FIGURES exit from a RUSTY DOOR.
+
+ZHELNA
+We've got them. You ready?
+
+Dekka nods. Before the pair can act, the figures have already split in four different directions, slipping their way through the crowd with urgency.
+
+DEKKA
+Shit.
+
+ZHELNA
+That one. I've got him. Back me up.
+
+Zhelna points to the northernmost figure, who is weaving away from their location towards the CENTRAL THOROUGHFARE.
+
+Activating her MASK, it slides into place and covers her head completely, the panels meshing together like a robotic jigsaw. The crimson LENSES that protrude from in front of her eyes pulsate.
+
+She jumps, her jetpack activating with a sharp WOOSH. Zhelna twists her hand, revealing a set of three SURGICAL SCALPELS, each THRUMMING with a laser-like energy that wraps around their blades. Flicking her arm, the scalpels fly through the air. They hit the figure's calf with a CRACK, sending him tumbling to the ground.
+
+Zhelna activates a CLOAKING DEVICE a few metres before deftly hitting the ground, disguising her entry into the crowd. She uncloacks, rushing towards a gap in the crowd that has now opened around the injured figure, his BLACK ROBES now stained with blood. He attempts to stand but cannot.
+
+ZHELNA
+(Pushing through the crowd, in an empathetic voice)
+Is someone hurt? Coming through, excuse me.
+
+Zhelna makes it into the parting created in the crowd.
+
+ZHELNA (CONT'D)
+Everything is okay, please stay clam. I'm a Midwife - I'm trained to handle medical emergencies. Are you ok? Let's treat you somewhere less exposed.
+
+As she goes to lift the figure up, he tries to resist. She reacts, discretely pulling a HIDDEN BLADE from her sleeve, pushing it against the small of his back.
+
+ZHELNA (CONT'D)
+Coming through! Make way, please.
+
+Dekka also lands, uncloaking a few metres from the commotion. He catches up with Zhelna who is carrying the masked figure towards the warehouse.
+
+DEKKA
+(Under his breath)
+Everything go to plan?
+
+ZHELNA
+Well, I just egregiously broke my hypocratic oath, but apart from that - yeah, we're good. (pause) He's a bit of a wriggler, watch him.
+
+INT. WAREHOUSE - NIGHT`
